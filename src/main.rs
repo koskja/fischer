82d2e6e1185c1967e9619e41ac70bf421f984850
koskja <87826472+koskja@users.noreply.github.com>
2025-11-17 15:38:42 +0100

    Background subtraction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(let_chains)]
+#![feature(random)]
 mod control;
 mod recog;
 
--- src/recog.rs
+++ src/recog.rs
@@ -1,10 +1,14 @@
 use std::{
-    sync::mpsc::{Receiver, SyncSender},
+    random,
+    sync::{
+        mpsc::{Receiver, SyncSender},
+        OnceLock,
+    },
     time::{Duration, Instant},
 };
 
 use eyre::Context;
-use image::{Rgb, RgbImage};
+use image::{Pixel as _, Rgb, RgbImage};
 
 use crate::control::{ToBrain, ToController, ToEyes};
 
@@ -49,30 +53,84 @@ fn rgb_to_hsv(rgb: Rgb<u8>) -> (f64, f64, f64) {
     (hue, saturation, value)
 }
 
-/// This function finds the center of mass of pixels with a cyan value < 10% and saturation > 40%.
-pub fn find_bobber(img: &RgbImage) -> Option<(i32, i32)> {
+fn ms_since_start() -> u64 {
+    static START: OnceLock<Instant> = OnceLock::new();
+    START.get_or_init(|| Instant::now()).elapsed().as_millis() as u64
+}
+
+/// This function finds the center of mass of pixels that fall within a specific
+/// HSV color range, tuned to find the red part of the fishing bobber.
+pub fn find_bobber(img: &RgbImage, background: &RgbImage) -> Option<(i32, i32)> {
     let mut total_x = 0.0;
     let mut total_y = 0.0;
     let mut count = 0;
 
+    assert_eq!(img.dimensions(), background.dimensions());
+    let mut output = img.clone();
+    let mut output_hsv = img.clone();
+    let mut output_cmyk = img.clone();
+    let mut output_detected_pixels = img.clone();
+
     let w = img.width();
     let h = img.height();
-    for (x, y, pixel) in img.enumerate_pixels() {
-        if w / 3 > x || 2 * w / 3 < x || h / 3 > y || 2 * h / 3 < y {
-            continue;
-        }
-        let (cyan, _, _, _) = rgb_to_cmyk(*pixel);
-        let (_, saturation, _) = rgb_to_hsv(*pixel);
+    let left = w / 3;
+    let right = 2 * w / 3;
+    let top = h / 3;
+    let bottom = 2 * h / 3;
 
-        let cyan_threshold = 0.1;
-        let saturation_threshold = 0.4;
+    for y in top..bottom {
+        for x in left..right {
+            let foreground_pixel = img.get_pixel(x, y);
+            let background_pixel = background.get_pixel(x, y);
+            let pixel =
+                foreground_pixel.map2(background_pixel, |a, b| (a as i32 - b as i32).abs() as u8);
+            output.put_pixel(x, y, pixel);
+            let (cyan, m, yellow, _) = rgb_to_cmyk(pixel);
+            let (h, saturation, v) = rgb_to_hsv(pixel);
+            output_cmyk.put_pixel(
+                x,
+                y,
+                Rgb([
+                    (cyan * 255.0) as u8,
+                    (m * 255.0) as u8,
+                    (yellow * 255.0) as u8,
+                ]),
+            );
+            output_hsv.put_pixel(
+                x,
+                y,
+                Rgb([
+                    (h / 360.0 * 255.0) as u8,
+                    (saturation * 255.0) as u8,
+                    (v * 255.0) as u8,
+                ]),
+            );
 
-        // Check if the pixel meets the criteria
-        if cyan < cyan_threshold && saturation > saturation_threshold {
-            total_x += x as f64;
-            total_y += y as f64;
-            count += 1;
+            // HSV thresholding for red color
+            let is_red = (h < 20.0 || h > 340.0) && saturation > 0.5 && v > 0.3;
+
+            if is_red {
+                total_x += x as f64;
+                total_y += y as f64;
+                count += 1;
+                output_detected_pixels.put_pixel(x, y, Rgb([255, 0, 0]));
+            }
         }
+    }
+
+    if false && random::random::<u8>() % 100 == 0 {
+        output
+            .save(format!("output_{}.png", ms_since_start()))
+            .unwrap();
+        output_cmyk
+            .save(format!("output_cmyk_{}.png", ms_since_start()))
+            .unwrap();
+        output_hsv
+            .save(format!("output_hsv_{}.png", ms_since_start()))
+            .unwrap();
+        output_detected_pixels
+            .save(format!("output_detected_pixels_{}.png", ms_since_start()))
+            .unwrap();
     }
 
     if count > 0 {
@@ -100,7 +158,7 @@ impl HookCast {
     pub fn register_pos(&mut self, [nx, ny]: [i32; 2]) -> bool {
         if (Instant::now() - self.start).as_millis() > 3000 {
             if let Some([bx, by]) = self.bobber_pos {
-                (bx - nx).pow(2) + (by - ny).pow(2) > 25
+                (bx - nx).pow(2) + (by - ny).pow(2) > 10i32.pow(2)
             } else {
                 self.bobber_pos = Some([nx, ny]);
                 false
@@ -112,10 +170,16 @@ impl HookCast {
 }
 pub struct Brain {
     ongoing: Option<HookCast>,
+    reference_image: Option<RgbImage>,
+    stopped_casting_at: Option<Instant>,
 }
 impl Brain {
     pub fn new() -> Self {
-        Self { ongoing: None }
+        Self {
+            ongoing: None,
+            reference_image: None,
+            stopped_casting_at: None,
+        }
     }
     pub fn cast(&mut self) -> eyre::Result<ToController> {
         self.ongoing = Some(HookCast::new());
@@ -130,48 +194,65 @@ impl Brain {
         let log_frames = false;
         let timestamp = Instant::now();
         loop {
-            if self.ongoing.is_none() {
-                output.send(self.cast()?)?;
-                continue;
-            }
-            if let Some(cast) = &self.ongoing
-                && Instant::now() - cast.start > Duration::from_secs(30)
-            {
-                output.send(self.cast()?)?;
-                continue;
+            if self.reference_image.is_some() {
+                if self.ongoing.is_none() {
+                    output.send(self.cast()?)?;
+                    continue;
+                }
+                if let Some(cast) = &self.ongoing
+                    && Instant::now() - cast.start > Duration::from_secs(30)
+                {
+                    output.send(self.cast()?)?;
+                    continue;
+                }
             }
             let frame = input.recv().wrap_err("Failed to receive next input")?;
-            match frame {
-                ToBrain::NextFrame(mut frame) => {
-                    if let Some((x, y)) = find_bobber(&frame) {
-                        if log_frames {
-                            for i in -10..=10 {
-                                for j in -10..=10 {
-                                    frame.put_pixel(
-                                        (x + i) as u32,
-                                        (y + j) as u32,
-                                        Rgb([255, 0, 0]),
-                                    );
-                                }
+            'anchor: loop {
+                match frame {
+                    ToBrain::NextFrame(mut frame) => {
+                        if self.reference_image.is_none() {
+                            if self.stopped_casting_at.is_none()
+                                || Instant::now() - self.stopped_casting_at.unwrap()
+                                    > Duration::from_secs(3)
+                            {
+                                self.reference_image = Some(frame);
                             }
-                            frame
-                                .save(format!("frame_{}.png", timestamp.elapsed().as_millis()))
-                                .unwrap();
+                            break 'anchor;
                         }
-                        output.send(ToController::MoveMouse([x, y]))?;
-                        if self.ongoing.as_mut().unwrap().register_pos([x, y]) {
-                            output.send(ToController::PerformClick([x, y]))?;
-                            self.ongoing = None;
-                            println!("Bere!");
+                        if let Some((x, y)) =
+                            find_bobber(&frame, self.reference_image.as_ref().unwrap())
+                        {
+                            if log_frames {
+                                for i in -10..=10 {
+                                    for j in -10..=10 {
+                                        frame.put_pixel(
+                                            (x + i) as u32,
+                                            (y + j) as u32,
+                                            Rgb([255, 0, 0]),
+                                        );
+                                    }
+                                }
+                                frame
+                                    .save(format!("frame_{}.png", timestamp.elapsed().as_millis()))
+                                    .unwrap();
+                            }
+                            output.send(ToController::MoveMouse([x, y]))?;
+                            if self.ongoing.as_mut().unwrap().register_pos([x, y]) {
+                                output.send(ToController::PerformClick([x, y]))?;
+                                self.ongoing = None;
+                                self.reference_image = None;
+                                self.stopped_casting_at = Some(Instant::now());
+                                println!("Bere!");
+                            }
                         }
                     }
-                }
-            };
+                };
+                break;
+            }
             eyes_feedback.send(ToEyes::FrameProcessed)?;
         }
     }
 }
-
 impl Default for Brain {
     fn default() -> Self {
         Self::new()
